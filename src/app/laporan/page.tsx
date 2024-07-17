58637d33b231e5fa46087172a6c33bf747fa1a31
galuhsatria <galuhsatria219@gmail.com>
2024-07-17 09:56:36 +0800

    update pages

---
+++ src/app/laporan/page.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { Input } from '../../components/ui/input';
+import { Textarea } from '../../components/ui/textarea';
+
+export default function Page() {
+  return (
+    <div className="pt-28 layout">
+      <div className="text-center">
+        <h1 className="font-bold text-2xl">Laporan Lokasi Blank Spot</h1>
+        <h2>Lengkapi data berikut untuk melaporkan lokasi atau daerah blank spot</h2>
+      </div>
+
+      <div>
+        <form action="" className="flex w-full gap-5 mt-10">
+          <div className="flex flex-col gap-2 w-full">
+            <div className="flex flex-col gap-3">
+              <label htmlFor="nama">Nama</label>
+              <Input type="text" placeholder="Masukkan nama anda" className="w-full focus:border-[#FBD46D] focus:outline-none outline-none transition-all duration-150" />
+            </div>
+            <div className="flex flex-col gap-3">
+              <label htmlFor="email">Email</label>
+              <Input type="text" placeholder="Masukkan email anda" className="w-full focus:border-[#FBD46D] focus:outline-none outline-none transition-all duration-150" />
+            </div>
+            <div className="flex flex-col gap-3">
+              <label htmlFor="nama">Telepon</label>
+              <Input type="text" placeholder="Masukkan nama anda" className="w-full focus:border-[#FBD46D] focus:outline-none outline-none transition-all duration-150" />
+            </div>
+            <div className="flex flex-col gap-3">
+              <label htmlFor="nama">Nama</label>
+              <Input type="text" placeholder="Masukkan nama anda" className="w-full focus:border-[#FBD46D] focus:outline-none outline-none transition-all duration-150" />
+            </div>
+          </div>
+          <div className="w-full h-full">
+            <div className="flex flex-col gap-3">
+              <label htmlFor="keterangan">Keterangan</label>
+              <Textarea placeholder="Masukkan keterangan" className="w-full focus:border-[#FBD46D] focus:outline-none outline-none transition-all duration-150" />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
