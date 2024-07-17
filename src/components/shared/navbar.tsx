bd3ab0630a2b96c24942509f3f2a63a2ed917360
galuhsatria <galuhsatria219@gmail.com>
2024-07-17 09:17:39 +0800

    add markers for the map

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -5,9 +5,11 @@ import Link from 'next/link';
 import React, { useState } from 'react';
 import { Button } from '../ui/button';
 import { AlignRight } from 'lucide-react';
+import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
-  const [isOpen, settIsopen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
 
   return (
     <header className="primary-color z-10 fixed w-full">
@@ -18,30 +20,30 @@ export default function Navbar() {
           </Link>
         </div>
         <div
-          className={`menu max-md:absolute max-md:py-8 max-md:-z-10  max-md:text-center max-md:bg-[#FBD46D] max-md:top-[4.2rem] transition-all  duration-200 max-md:border-t max-md:w-full max-md:left-0 ${
-            isOpen ? 'max-md:block' : 'max-md:-top-64'
-          } `}
+          className={`menu max-md:absolute max-md:py-8 max-md:-z-10 max-md:h-screen max-md:text-center max-md:bg-[#FBD46D] max-md:top-[4.2rem] transition-all duration-200 max-md:right-0 max-md:border-t max-md:w-full ${
+            isOpen ? 'max-md:block' : 'max-md:opacity-0'
+          }`}
         >
-          <ul className="flex gap-2 items-center max-md:flex-col max-md:gap-6">
-            {links.map(({ link, label }, index) => (
+          <ul className="flex gap-2 items-center max-md:flex-col max-md:gap-10">
+            {links.map(({ link, label, path }, index) => (
               <li key={index}>
-                <Link href={link} className="hover:bg-[#002E5B] hover:text-white px-6 rounded-full py-2 transition-all duration-200">
+                <Link href={link} className={`hover:bg-[#002E5B] hover:text-white px-6 rounded-full py-2 transition-all duration-200 ${pathname === label || pathname === path  ? 'bg-[#002E5B] text-white' : ''}`}>
                   {label}
                 </Link>
               </li>
             ))}
-            <Button variant={'default'} className="bg-[#002E5B] md:hidden w-max  hover:bg-[#002e5b] hover:bg-opacity-90  rounded-full text-white px-8">
-            Login
-          </Button>
+            <Button variant="default" className="bg-[#002E5B] md:hidden w-max hover:bg-[#002e5b] hover:bg-opacity-90 rounded-full text-white px-8">
+              Login
+            </Button>
           </ul>
         </div>
         <div className="max-md:hidden">
-          <Button variant={'default'} className="bg-[#002E5B] hover:bg-[#002e5b] hover:bg-opacity-90  rounded-full text-white px-8">
+          <Button variant="default" className="bg-[#002E5B] hover:bg-[#002e5b] hover:bg-opacity-90 rounded-full text-white px-8">
             Login
           </Button>
         </div>
 
-        <div className="cursor-pointer md:hidden" onClick={() => settIsopen(!isOpen)}>
+        <div className="cursor-pointer md:hidden" onClick={() => setIsOpen(!isOpen)}>
           <AlignRight />
         </div>
       </nav>
@@ -52,14 +54,17 @@ export default function Navbar() {
 const links = [
   {
     link: '/',
+    path:'/',
     label: 'Beranda',
   },
   {
     link: '/laporan',
+    path:'/laporan',
     label: 'Laporan',
   },
   {
     link: '/about',
+    path:'/about',
     label: 'About',
   },
 ];
