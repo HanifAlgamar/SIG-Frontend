5d0db5f8771cc9386471ddcd84af9ac5fb413f30
galuhsatria <galuhsatria219@gmail.com>
2024-07-18 11:14:27 +0800

    add function to search location

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,14 +1,15 @@
 'use client';
 
-import React from 'react';
-import { Search } from 'lucide-react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Input } from '../../components/ui/input';
-import { Button } from '../../components/ui/button';
-import { APIProvider, Map, MapCameraChangedEvent } from '@vis.gl/react-google-maps';
+import { APIProvider, ControlPosition, MapControl, AdvancedMarker, Map, useMap, useMapsLibrary, useAdvancedMarkerRef, MapCameraChangedEvent } from '@vis.gl/react-google-maps';
 import { PoiMarkers } from '@/components/shared/poimarker';
 import { locations } from '../dummydata/data';
 
 export default function Home() {
+  const [selectedPlace, setSelectedPlace] = useState<google.maps.places.PlaceResult | null>(null);
+  const [markerRef, marker] = useAdvancedMarkerRef();
+
   return (
     <main className="">
       <div>
@@ -18,26 +19,24 @@ export default function Home() {
         </div>
 
         <div className="flex layout flex-col items-center justify-center my-10 mx-4">
-          <div className="mb-3 justify-items-end w-full max-w-5xl">
-            <label htmlFor="search">Cari Lokasi Menara</label>
-            <div className="flex gap-4 mt-3 w-full">
-              <Input type="text" placeholder="Cari lokasi berdasarkan nama kota" className="w-full focus:border-[#FBD46D] focus:outline-none outline-none transition-all duration-150" />
-              <Button className="bg-[#002E5B]">
-                <Search />
-              </Button>
-            </div>
-          </div>
           <div className="w-full max-w-5xl">
             <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''}>
               <Map
                 defaultZoom={13}
                 defaultCenter={{ lat: -8.583333, lng: 116.116667 }}
                 onCameraChanged={(ev: MapCameraChangedEvent) => console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)}
-                mapId="DEMO_MAP_ID"
+                mapId="semidi2"
                 style={{ width: '100%', height: '400px' }}
               >
                 <PoiMarkers pois={locations} />
+                <AdvancedMarker ref={markerRef} position={null} />
               </Map>
+              <MapControl position={ControlPosition.TOP}>
+                <div className="autocomplete-control mt-6">
+                  <PlaceAutocomplete onPlaceSelect={setSelectedPlace} />
+                </div>
+              </MapControl>
+              <MapHandler place={selectedPlace} marker={marker} />
             </APIProvider>
           </div>
         </div>
@@ -45,3 +44,60 @@ export default function Home() {
     </main>
   );
 }
+
+interface MapHandlerProps {
+  place: google.maps.places.PlaceResult | null;
+  marker: google.maps.marker.AdvancedMarkerElement | null;
+}
+
+const MapHandler = ({ place, marker }: MapHandlerProps) => {
+  const map = useMap();
+
+  useEffect(() => {
+    if (!map || !place || !marker) return;
+
+    if (place.geometry?.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else if (place.geometry?.location) {
+      map.setCenter(place.geometry.location);
+      map.setZoom(15);
+    }
+    marker.position = place.geometry?.location;
+  }, [map, place, marker]);
+
+  return null;
+};
+
+interface PlaceAutocompleteProps {
+  onPlaceSelect: (place: google.maps.places.PlaceResult | null) => void;
+}
+
+const PlaceAutocomplete = ({ onPlaceSelect }: PlaceAutocompleteProps) => {
+  const [placeAutocomplete, setPlaceAutocomplete] = useState<google.maps.places.Autocomplete | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const places = useMapsLibrary('places');
+
+  useEffect(() => {
+    if (!places || !inputRef.current) return;
+
+    const options = {
+      fields: ['geometry', 'name', 'formatted_address'],
+    };
+
+    setPlaceAutocomplete(new places.Autocomplete(inputRef.current, options));
+  }, [places]);
+
+  useEffect(() => {
+    if (!placeAutocomplete) return;
+
+    placeAutocomplete.addListener('place_changed', () => {
+      onPlaceSelect(placeAutocomplete.getPlace());
+    });
+  }, [onPlaceSelect, placeAutocomplete]);
+
+  return (
+    <div className="autocomplete-container">
+      <Input type="text" placeholder="Cari lokasi" className="w-96 focus:border-[#FBD46D] focus:outline-none outline-none transition-all duration-150" ref={inputRef} />
+    </div>
+  );
+};
